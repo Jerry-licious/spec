2cea3a88cf45077ba21bfdf26d92f3beddfa17c1
Bokuan Li <bokuan.li@mail.mcgill.ca>
2026-02-15 16:03:49 -0500

    Added a commonly used array.

--- src/parser/util.ts
+++ src/parser/util.ts
@@ -2,6 +2,10 @@ import {Argument, Macro, Node} from "@unified-latex/unified-latex-types";
 import {NodeContext} from "./error";
 import { printRaw } from "@unified-latex/unified-latex-util-print-raw";
 
+
+export const documentDividers = ['part', 'chapter', 'section', 'subsection', 'subsubsection'] as const;
+
+
 export function getContext(node: Node): NodeContext | undefined {
     if (node.meta && node.meta.sourceFile && node.position) {
         return {

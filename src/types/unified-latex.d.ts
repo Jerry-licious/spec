600b36a16332e1c6584c66159f0e89407ac46aa8
Bokuan Li <bokuan.li@mail.mcgill.ca>
2026-02-14 22:37:49 -0500

    Augmented all node interfaces with a meta field.

--- src/types/unified-latex.d.ts
+++ src/types/unified-latex.d.ts
@@ -6,11 +6,46 @@ export interface ElementMetadata {
     numbering?: number[]
 }
 
+
+// I'm not sure why they don't export the base node type, but this will have to do for now.
 declare module '@unified-latex/unified-latex-types/libs/ast-types' {
+    interface ContentNode {
+        meta?: ElementMetadata;
+    }
     interface Macro {
         meta?: ElementMetadata;
     }
+    interface Root {
+        meta?: ElementMetadata;
+    }
+    interface String {
+        meta?: ElementMetadata;
+    }
+    interface Whitespace {
+        meta?: ElementMetadata;
+    }
+    interface Parbreak {
+        meta?: ElementMetadata;
+    }
     interface Environment {
+        meta?: ElementMetadata;
+    }
+    interface Comment {
+        meta?: ElementMetadata;
+    }
+    interface VerbatimEnvironment {
+        meta?: ElementMetadata;
+    }
+    interface InlineMath {
+        meta?: ElementMetadata;
+    }
+    interface DisplayMath {
+        meta?: ElementMetadata;
+    }
+    interface Group {
+        meta?: ElementMetadata;
+    }
+    interface Verb {
         meta?: ElementMetadata;
     }
 }

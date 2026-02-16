e5df083d190862323426c893aef872b2d1cd1681
Bokuan Li <bokuan.li@mail.mcgill.ca>
2026-02-16 11:45:08 -0500

    Added the block collector.

---
+++ src/parser/grouping/block-collector.ts
@@ -0,0 +1,42 @@
+import {DocumentVisitor} from "../visitor";
+import {Node} from "@unified-latex/unified-latex-types";
+import {VisitInfo} from "@unified-latex/unified-latex-util-visit";
+import {IRNode} from "./node";
+import {match} from "@unified-latex/unified-latex-util-match";
+import {BlockEnv} from "./block";
+
+
+export class BlockCollector extends DocumentVisitor {
+    // Both works as a set that contains all the theorem types to keep track of, and a
+    blockNames: Map<string, string>;
+    blocks: Map<number, IRNode>; // Map from tags to the created IR nodes.
+
+    constructor({ blockNames }: { blockNames: Map<string, string> }) {
+        super();
+
+        this.blockNames = blockNames;
+        this.blocks = new Map<number, IRNode>();
+    }
+
+    visit(node: Node, visitInfo: VisitInfo): void {
+        if (!(match.anyEnvironment(node) && this.blockNames.has(node.env))) return;
+
+        if (!node.meta || !node.meta.tag) {
+            this.addError('Missing metadata for block.');
+            return;
+        }
+
+        const blockName = this.blockNames.get(node.env)!!;
+        this.blocks.set(node.meta.tag, new BlockEnv({
+            name: blockName,
+            title: node.meta.title ?? [],
+            mainContent: node.content,
+            sourceNodeName: node.env,
+            label: node.meta.label,
+            tag: node.meta.tag,
+            numbering: node.meta.numbering ?? [],
+            proofs: node.meta.proofs ?? [],
+        }));
+    }
+}
+

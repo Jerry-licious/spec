879c5f8520c5e963c1cc5dde43e352709b3830d6
Bokuan Li <bokuan.li@mail.mcgill.ca>
2026-02-17 13:52:07 -0500

    Added event handlers and the success case for the logger.

--- src/parser/logging-base.ts
+++ src/parser/logging-base.ts
@@ -1,5 +1,6 @@
 // Abstractly processes certain things. While running, collects errors.
 import {ParsingMessage} from "./error";
+import {on} from "node:events";
 
 export class ParserLogger {
     readonly parent?: ParserLogger;
@@ -7,13 +8,31 @@ export class ParserLogger {
     readonly errors: ParsingMessage[];
     readonly warnings: ParsingMessage[];
     readonly infos: ParsingMessage[];
+    readonly successes: ParsingMessage[];
 
-    constructor({ parent }: { parent?: ParserLogger }) {
+    readonly onError?: (message: ParsingMessage) => void;
+    readonly onWarning?: (message: ParsingMessage) => void;
+    readonly onInfo?: (message: ParsingMessage) => void;
+    readonly onSuccess?: (message: ParsingMessage) => void;
+
+    constructor({ parent, onError, onWarning, onInfo, onSuccess }: {
+        parent?: ParserLogger,
+        onError?: (message: ParsingMessage) => void;
+        onWarning?: (message: ParsingMessage) => void;
+        onInfo?: (message: ParsingMessage) => void;
+        onSuccess?: (message: ParsingMessage) => void;
+    }) {
         this.errors = [];
         this.warnings = [];
         this.infos = [];
+        this.successes = [];
 
         this.parent = parent;
+
+        this.onError = onError;
+        this.onWarning = onWarning;
+        this.onInfo = onInfo;
+        this.onSuccess = onSuccess;
     }
 
     addError(err: ParsingMessage) {
@@ -21,17 +40,35 @@ export class ParserLogger {
         if (this.parent) {
             this.parent.addError(err);
         }
+        if (this.onError) {
+            this.onError(err);
+        }
     }
     addWarning(warning: ParsingMessage) {
         this.warnings.push(warning);
         if (this.parent) {
             this.parent.addWarning(warning);
         }
+        if (this.onWarning) {
+            this.onWarning(warning);
+        }
     }
     addInfo(info: ParsingMessage) {
         this.infos.push(info);
         if (this.parent) {
             this.parent.addInfo(info);
+        }
+        if (this.onInfo) {
+            this.onInfo(info);
+        }
+    }
+    addSuccess(success: ParsingMessage) {
+        this.successes.push(success);
+        if (this.parent) {
+            this.parent.addSuccess(success);
+        }
+        if (this.onSuccess) {
+            this.onSuccess(success);
         }
     }
 }

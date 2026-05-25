b9f5278ede03ef7b851aeaa1ef7504483f551674
Bokuan Li <bokuan.li@mail.mcgill.ca>
2026-05-25 14:30:23 -0400

    Fixed a bug where every list item collapses in sync.

--- src/components/LinkList.tsx
+++ src/components/LinkList.tsx
@@ -16,29 +16,34 @@ export interface LinkListProps {
     defaultCollapsed?: boolean,
 }
 
-export function LinkList(props: LinkListProps) {
-    const [collapsed, setCollapsed] = createSignal(props.defaultCollapsed ?? false);
+function LinkedListItem(item: LinkListItem) {
+    const [collapsed, setCollapsed] = createSignal(item.defaultCollapsed ?? false);
 
+    return (<li class={'link-list-item'}>
+        {
+            item.children.length ? <button class={'collapse-button'}
+                                           onClick={() => setCollapsed((c) => !c)}>{
+                collapsed() ? 'keyboard_arrow_right' : 'keyboard_arrow_down'
+            }</button> : <span class={'link-list-marker'}>circle</span>
+        }
+        {
+            typeof item.content === 'string' ? <a href={item.href} class={'link-primary'} innerHTML={item.content}/> :
+                <a href={item.href} class={'link-primary'}>{item.content}</a>
+        }
+        <Show when={item.children.length && !collapsed()}>
+            {
+                item.children.length ? <LinkList items={item.children} defaultCollapsed={item.defaultCollapsed}/> : null
+            }
+        </Show>
+    </li>)
+}
+
+
+export function LinkList(props: LinkListProps) {
     return <div class={'link-list-container'}>
         { props.title? <h3>{props.title}</h3> : null }
         <ul class={'link-list'}>
-            {props.items.map(item => <li class={'link-list-item'}>
-                {
-                    item.children.length ? <button class={'collapse-button'}
-                                                   onClick={() => setCollapsed((c) => !c)}>{
-                        collapsed() ? 'keyboard_arrow_right' : 'keyboard_arrow_down'
-                    }</button> : <span class={'link-list-marker'}>circle</span>
-                }
-                {
-                    typeof item.content === 'string' ? <a href={item.href} class={'link-primary'} innerHTML={item.content}/> :
-                        <a href={item.href} class={'link-primary'}>{item.content}</a>
-                }
-                <Show when={item.children.length && !collapsed()}>
-                    {
-                        item.children.length ? <LinkList items={item.children} defaultCollapsed={item.defaultCollapsed}/> : null
-                    }
-                </Show>
-            </li>)}
+            {props.items.map(item => <LinkedListItem {...item}/>)}
         </ul>
     </div>
 }
